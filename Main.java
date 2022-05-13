0dd18952da793a0007035f14b6a4e6ccc2425d86
DaniAlves27 <daniel.alves08@outlook.com>
2022-05-13 20:54:12 -0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package sobrecargaJava;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("-----Sobrecarga------");
+		quadrilatero.area(5);
+		quadrilatero.area(4, 5);
+		quadrilatero.area(5, 10, 15);
+
+	}
+
+}
---
+++ quadrilatero.java
@@ -0,0 +1,28 @@
+package sobrecargaJava;
+
+public class quadrilatero {
+
+	public static void area(int l) {
+
+		double areaQ = l * l;
+
+		System.out.println("A área do quadrado é : " + areaQ);
+
+	}
+
+	public static void area(double b, double h) {
+
+		double areaR = b * h;
+
+		System.out.println("A área do retângulo é : " + areaR);
+
+	}
+
+	public static void area(double bMaior, double bMenor , double aLt) {
+
+		double areaT = (bMaior + bMenor) * aLt / 2;
+
+		System.out.println("A área do trapézio é : " + areaT);
+
+	}
+}
